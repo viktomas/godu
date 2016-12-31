915a550fd32078c5886df4190c499b2a46d6b5be
Tomas Vik <vicek22@gmail.com>
2016-12-31 13:36:04 +1100

    Implemented EnterComand and wrote failing test for GoBack
    - and then I realised that my State struct is not good enough
    - I need to create a stack of ancestors or something similar

--- core/command.go
+++ core/command.go
@@ -1,13 +1,38 @@
 package core
 
+import "fmt"
+
 // State represents system configuration after processing user input
 type State struct {
 	parent   *File
 	folder   *File
-	history  map[*File]int
-	selected int
 	filepath string
 }
 
-// Command represents a user action triggered on a State
-type Command func(State) State
+// Executer represents a user action triggered on a State
+type Executer interface {
+	Execute(State) (State, error)
+}
+
+type Enter struct {
+	index int
+}
+
+type GoBack struct{}
+
+func (e Enter) Execute(oldState State) (State, error) {
+	if e.index < 0 || e.index >= len(oldState.folder.Files) {
+		return oldState, fmt.Errorf("Trying to enter nonexistent folder on index %d", e.index)
+	}
+	newFolder := oldState.folder.Files[0]
+	newFilepath := fmt.Sprintf("%s%s/", oldState.filepath, newFolder.Name)
+	return State{
+		parent:   oldState.folder,
+		folder:   newFolder,
+		filepath: newFilepath,
+	}, nil
+}
+
+func (GoBack) Execute(oldState State) (State, error) {
+	return oldState, nil
+}
--- core/command_test.go
+++ core/command_test.go
@@ -1,1 +1,69 @@
 package core
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEnterCommand(t *testing.T) {
+	subTree := &File{"b", 50, []*File{
+		&File{"c", 50, []*File{}},
+	}}
+	tree := &File{"a", 50, []*File{
+		subTree,
+	}}
+	initialState := State{
+		folder:   tree,
+		filepath: "a/",
+	}
+	command := Enter{0}
+	newState, _ := command.Execute(initialState)
+	expectedState := State{
+		parent:   tree,
+		folder:   subTree,
+		filepath: "a/b/",
+	}
+	if !reflect.DeepEqual(newState, expectedState) {
+		t.Errorf("New state is not same as expected %v and %v", newState, expectedState)
+	}
+}
+
+func TestEnterCommandFails(t *testing.T) {
+	tree := &File{"a", 50, []*File{
+		&File{"b", 50, []*File{
+			&File{"c", 50, []*File{}},
+		}},
+	}}
+	initialState := State{
+		folder: tree,
+	}
+	command := Enter{1}
+	_, err := command.Execute(initialState)
+	if err == nil {
+		t.Error("Command Enter didn't fail when trying to enter nonexistent folder")
+	}
+
+}
+
+func TestGoBackCommand(t *testing.T) {
+	subTree := &File{"b", 50, []*File{
+		&File{"c", 50, []*File{}},
+	}}
+	tree := &File{"a", 50, []*File{
+		subTree,
+	}}
+	initialState := State{
+		parent:   tree,
+		folder:   subTree,
+		filepath: "a/b/",
+	}
+	command := GoBack{}
+	newState, _ := command.Execute(initialState)
+	expectedState := State{
+		folder:   tree,
+		filepath: "a/",
+	}
+	if !reflect.DeepEqual(newState, expectedState) {
+		t.Errorf("New state is not same as expected %v and %v", newState, expectedState)
+	}
+}
