7d90dc2a09cccfc52898e28490021ae25e38669b
Tomas Vik <vicek22@gmail.com>
2017-01-01 18:54:34 +1100

    Adding flag -l to allow for customising the file limit.

--- godu.go
+++ godu.go
@@ -10,11 +10,12 @@ import (
 )
 
 func main() {
+	limit := flag.Int64("l", 10, "show only files larger than limit (in MB)")
 	flag.Parse()
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
 	tree := core.GetSubTree(roots[0], ioutil.ReadDir, getIgnoredFolders())
-	interactive.InteractiveTree(&tree, os.Stdout, os.Stdin, 10*1024*1024)
+	interactive.InteractiveTree(&tree, os.Stdout, os.Stdin, *limit*core.MEGABYTE)
 }
