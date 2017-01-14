62a601171d3bad2049276755213abd34c0fa659c
Tomas Vik <tomas.vik@rea-group.com>
2017-01-14 20:01:12 +1100

    Rewriting PrintMarkedFiles function
    - sorting by length (if this is used for manipulating/deleting files,
      subfolders and subfiles are always manipulated first)

--- interactive/printer.go
+++ interactive/printer.go
@@ -2,13 +2,28 @@ package interactive
 
 import (
 	"fmt"
-	"io"
+	"sort"
 
 	"github.com/viktomas/godu/core"
 )
 
-func PrintMarkedFiles(state *core.State, writer io.Writer) {
-	for file := range state.MarkedFiles {
-		fmt.Fprintf(writer, "'%s'\n", file.Path())
+type byLength []string
+
+func (l byLength) Len() int           { return len(l) }
+func (l byLength) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l byLength) Less(i, j int) bool { return len(l[i]) > len(l[j]) }
+
+// QuoteMarkedFiles takes files from the map and returns slice of qoted file paths
+func QuoteMarkedFiles(markedFiles map[*core.File]struct{}) []string {
+	quotedFiles := make([]string, len(markedFiles))
+	i := 0
+	for file := range markedFiles {
+		quotedFiles[i] = fmt.Sprintf("'%s'", file.Path())
+		i++
 	}
+	// sorting lenght of the path (assuming that we want to deleate files in subdirs first)
+	// alfabetical sorting added for deterinism (map keys doesn't guarantee order)
+	sort.Sort(sort.StringSlice(quotedFiles))
+	sort.Sort(byLength(quotedFiles))
+	return quotedFiles
 }
--- interactive/printer_test.go
+++ interactive/printer_test.go
@@ -1,26 +1,17 @@
 package interactive
 
 import (
-	"bufio"
-	"bytes"
 	"reflect"
-	"sort"
-	"strings"
 	"testing"
 
 	"github.com/viktomas/godu/core"
 )
 
-func TestPrintMarkedFilesNone(t *testing.T) {
+func TestPrintEmptyMarkedFiles(t *testing.T) {
 	marked := make(map[*core.File]struct{})
-	state := &core.State{MarkedFiles: marked}
-	var buffer bytes.Buffer
-	writer := bufio.NewWriter(&buffer)
-	defer writer.Flush()
-	PrintMarkedFiles(state, writer)
-	result := buffer.String()
-	if result != "" {
-		t.Errorf("Expected empty output from PrintMarkedFiles, got '%s'", result)
+	result := QuoteMarkedFiles(marked)
+	if len(result) > 0 {
+		t.Errorf("Expected empty output from PrintMarkedFiles, got '%v'", result)
 	}
 }
 
@@ -40,24 +31,9 @@ func TestPrintMarkedFiles(t *testing.T) {
 	marked[core.FindTestFile(root, "d2")] = struct{}{}
 	marked[core.FindTestFile(root, "f1")] = struct{}{}
 	marked[core.FindTestFile(root, "f2")] = struct{}{}
-	//marked[getFileByName(root, "f3")] = struct{}{}
-	state := &core.State{MarkedFiles: marked}
-	var buffer bytes.Buffer
-	writer := bufio.NewWriter(&buffer)
-	PrintMarkedFiles(state, writer)
-	writer.Flush()
-	result := buffer.String()
-	// We don't know the order, as we are using a map to store marked files :/
-	expected := "'d1'\n'd1/f1'\n'd2'\n'd1/d3/f2'\n"
-	if !hasSameLines(result, expected) {
-		t.Errorf("Expected '%s' from PrintMarkedFiles, got '%s'", expected, result)
+	result := QuoteMarkedFiles(marked)
+	expected := []string{"'d1/d3/f2'", "'d1/f1'", "'d1'", "'d2'"}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected '%v' from PrintMarkedFiles, got '%v'", expected, result)
 	}
-}
-
-func hasSameLines(value, expected string) bool {
-	values := strings.Split(value, "\n")
-	expecteds := strings.Split(expected, "\n")
-	sort.Sort(sort.StringSlice(values))
-	sort.Sort(sort.StringSlice(expecteds))
-	return reflect.DeepEqual(values, expecteds)
 }
